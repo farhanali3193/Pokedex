302750819c0b97bd483147b1ec3452969ac0fa56
farhanali3193 <61304043+farhanali3193@users.noreply.github.com>
2020-05-24 02:32:37 -0700

    Updates

---
+++ precache-manifest.8cfe1486fa8927c1c601b2fd87ce9f0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecfbd9d6328271da5d5acf6fd418ba69",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "ededbf7660244a650ca5",
+    "url": "/Pokedex/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "af45677ff3e35c0aff4c",
+    "url": "/Pokedex/static/css/main.45780015.chunk.css"
+  },
+  {
+    "revision": "ededbf7660244a650ca5",
+    "url": "/Pokedex/static/js/2.ec68025e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pokedex/static/js/2.ec68025e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af45677ff3e35c0aff4c",
+    "url": "/Pokedex/static/js/main.4b5a4873.chunk.js"
+  },
+  {
+    "revision": "1c8a54ccf81f15e85b43",
+    "url": "/Pokedex/static/js/runtime-main.75842f26.js"
+  }
+]);
